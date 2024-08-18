16eb4c065c4c8be32282147967d36b890816912b
hoshinzm <178138103+hoshinzm@users.noreply.github.com>
2024-08-18 12:58:01 +0900

    create memoList style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,39 +2,41 @@ import { View, Text, StyleSheet } from "react-native"
 
 const Index = (): JSX.Element => {
     return(
-        <View style = {styles.container}>
-            <View>
-                <View>
-                    <Text>Memo App</Text>
-                    <Text>ログアウト</Text>
+        <View style={styles.container}>
+
+            <View style={styles.header}>
+                <View style={styles.headerInner}>
+                    <Text style={styles.headerTitle}>Memo App</Text>
+                    <Text style={styles.headerRight}>ログアウト</Text>
                 </View>
             </View>
+
             <View>
                 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2024年 12月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2024年 12月1日 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
                     </View>
                 </View>
 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2024年 12月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2024年 12月1日 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
                     </View>
                 </View>
 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2024年 12月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2024年 12月1日 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
@@ -51,10 +53,56 @@ const Index = (): JSX.Element => {
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent: 'center', // 縦方向
-        alignItems: 'center' // 横方向
+    container: {
+        flex: 1, // 要素を画面全体に広げる設定
+        backgroundColor: '#ffffff'
+    },
+
+    header: {
+        backgroundColor: '#467FD3',
+        height: 104,
+        justifyContent: 'flex-end'
+    },
+
+    headerInner: {
+        alignItems: 'center'
+    },
+
+    headerTitle: {
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+
+    headerRight: {
+        position: 'absolute', //flexの設定から外れて好きな位置に要素を配置する
+        right: 16,
+        bottom: 16,
+        color: 'rgba(255,255,255,0.7)'
+    },
+
+    memoListItem: {
+        backgroundColor: '#ffffff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        paddingHorizontal: 19,
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: 'rgba(0,0,0,0.15)'
+    },
+
+    memoListItemTitle:{
+        fontSize: 16,
+        lineHeight: 32
+    },
+
+    memoListItemDate:{
+        fontSize: 12,
+        lineHeight: 16,
+        color: '#848484'
     }
 })
 
